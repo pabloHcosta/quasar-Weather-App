b8f9f12aff8d51b3f1ca0ca043ff00c0999fe4f9
pabloHaCosta <pablo.costa01@icloud.com>
2020-10-02 17:54:08 -0300

    add low and high temperature on display

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
       <q-input
       v-model="search"
       @keyup.enter="getWeatherBySearch()"
-      placeholder="Procurar"
+      placeholder="Search city"
       dark
       borderless
       >
@@ -34,16 +34,21 @@
           {{ weatherData.weather[0].main}}
         </div>
         <div class="text-h1 text-weight-thin
-        q-my-lg relative-position"
+        q-ma-lg relative-position "
         >
-        <span> {{ Math.round(weatherData.main.temp)}} </span>
-        <span class=" text-h4 relative-position
-        degree">&deg;C</span>
+        <span> {{ Math.round(weatherData.main.temp)}}</span>
+        <span class=" text-h4 relative-position degree">&deg;</span>
         </div>
+        <div class="text-h6 text-weight-light">
+          <span>H: {{ Math.round(weatherData.main.temp_max)}}&deg;</span>
+          <span>  -  </span>
+          <span>L: {{ Math.round(weatherData.main.temp_min)}}&deg;</span>
+        </div>
+        <div class="col text-center">
+          <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`">
+       </div>
       </div>
-      <div class="col text-center">
-        <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`">
-      </div>
+        <q-separator color="white"/>
     </template>
     <div class="col city">
     </div>
