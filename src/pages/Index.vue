96b7094b1e9da8af1e6be4b25a0c12abd22693c2
pabloHaCosta <pablo.costa01@icloud.com>
2020-10-02 23:39:19 -0300

    Menu name changed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-page class="flex column" >
+  <q-page class="flex column" :class="bgClass">
     <div class="col q-pt-lg q-px-md">
       <q-input
       v-model="search"
       @keyup.enter="getWeatherBySearch()"
-      placeholder="Search city"
+      placeholder="Search"
       dark
       borderless
       >
@@ -31,7 +31,7 @@
           {{ weatherData.name }}
         </div>
         <div class="text-h6 text-weight-light">
-          {{ weatherData.weather[0].main}}
+          {{ weatherData.weather[0].description}}
         </div>
         <div class="text-h1 text-weight-thin
         q-ma-lg relative-position "
@@ -48,7 +48,35 @@
           <img :src="`http://openweathermap.org/img/wn/${ weatherData.weather[0].icon }@2x.png`">
        </div>
       </div>
-        <q-separator color="white"/>
+      <q-separator  color="white"/>
+      <div class="row justify-around q-mx-lg q-mt-md">
+        <div class="col-4 text-gray text-weight-light">FEELS</div>
+        <div class="col-4 text-gray text-weight-light">HUMIDITY</div>
+      </div>
+      <div class="row justify-around q-mx-lg q-mb-md ">
+        <div class=" col-4 text-h6 text-white text-weight-light">
+          <span>{{ Math.round(weatherData.main.feels_like)}}&deg;</span>
+        </div>
+        <div class=" col-4 text-h6 text-white text-weight-light">
+          <span>{{ Math.round(weatherData.main.humidity)}}%</span>
+        </div>
+      </div>
+
+      <q-separator color="white" inset />
+
+      <div class="row justify-around q-mx-lg q-mt-md">
+        <div class="col-4 text-gray text-weight-light">WIND</div>
+        <div class="col-4 text-gray text-weight-light">PRESSURE</div>
+      </div>
+      <div class="row justify-around q-mx-lg ">
+        <div class=" col-4 text-h6 text-white text-weight-light">
+          <span>{{ Math.round(weatherData.wind.speed)}} km\hr</span>
+        </div>
+        <div class=" col-4 text-h6 text-white text-weight-light">
+          <span>{{ Math.round(weatherData.main.pressure)}} hPa</span>
+        </div>
+      </div>
+      <q-separator color="white"/>
     </template>
     <div class="col city">
     </div>
@@ -62,11 +90,27 @@ export default {
   data () {
     return {
       search: '',
+      sunriseTime: null,
+      sunsetTime: null,
       weatherData: null,
       lat: null,
       lon: null,
       apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
       apiKey: '7e2cf490879455b7214e9ba5390dc103'
+    }
+  },
+  computed: {
+    bgClass () {
+      var result = ''
+      if (this.weatherData) {
+        if (this.weatherData.weather[0].icon.endsWith('n')) {
+          result = 'bg-night'
+        } else {
+          result = 'bg-day'
+        }
+        console.log(result)
+      }
+      return result
     }
   },
 
@@ -94,14 +138,13 @@ export default {
 
     getWeatherByCoords () {
       axios
-        .get(`${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric&lang=pt_br`)
+        .get(`${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`)
         .then(response => (this.weatherData = response.data))
-      console.log(this.weatherData)
     },
 
     getWeatherBySearch () {
       axios
-        .get(`${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=metric&lang=pt_br`)
+        .get(`${this.apiUrl}?q=${this.search}&appid=${this.apiKey}&units=metric`)
         .then(response => (this.weatherData = response.data))
     }
   }
@@ -110,12 +153,11 @@ export default {
 
 <style lang="sass">
 .q-page
-  background: linear-gradient(to bottom, #136a8a, #267871 )
+  background: linear-gradient(to right, #2980b9, #6dd5fa, #ffffff)
+  &.bg-night
+  background: linear-gradient(to right, #000000, #434343)
+  &.bg-day
+  background: linear-gradient(to right, #007991, #78ffd6)
   .degree
     top: -44px
-  .city
-    flex: 0 0 100px
-    background: url(../assets/city.png)
-    background-size: contain
-    background-position: center bottom
 </style>
