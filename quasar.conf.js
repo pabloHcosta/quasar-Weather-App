9c9495055a5dc8ab6f7e2d0f89498edf38f48215
pabloHaCosta <pablo.costa01@icloud.com>
2020-10-02 23:41:27 -0300

    App name changed

--- quasar.conf.js
+++ quasar.conf.js
@@ -116,8 +116,8 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
+        name: 'Weather App',
+        short_name: 'Weather App',
         description: 'A Quasar Framework app',
         display: 'standalone',
         orientation: 'portrait',
